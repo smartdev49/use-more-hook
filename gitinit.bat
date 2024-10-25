b5802480d5e8f0fff851b012c1a584e0ef5357f4
unknown <curtisjowen@gmail.com>
2024-10-25 03:30:48 -0400

    first project

--- gitinit.bat
+++ gitinit.bat
@@ -1,5 +1,6 @@
 git init
 git add .
 git config user.email "curtisjowen@gmail.com"
+git commit -m "first project"
 git remote add origin git@github-curtis:smartdev49/use-more-hook.git
 git push --set-upstream origin master
